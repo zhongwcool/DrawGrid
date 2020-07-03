2ff83176add189aa4e90353bda594a6aee5e7c0e
Alex zhong <zhongwcool@163.com>
2020-07-03 09:57:31 +0800

    init commit

---
+++ DrawGrid/GridTool.cs
@@ -0,0 +1,112 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DrawGrid
+{
+    public class GridTool
+    {
+        public static void Draw(Panel panel)
+        {
+            SolidColorBrush gridBrush = new SolidColorBrush {Color = Colors.Red};
+
+            double scaleX = 30;
+            double currentPosY = 0;
+            currentPosY += scaleX;
+            while (currentPosY < panel.ActualHeight)
+            {
+                Line line = new Line
+                {
+                    X1 = 0,
+                    Y1 = currentPosY,
+                    X2 = panel.ActualWidth,
+                    Y2 = currentPosY,
+                    Stroke = gridBrush,
+                    StrokeThickness = 0.1
+                };
+                panel.Children.Add(line);
+                
+                currentPosY += scaleX;
+            }
+            
+            double scaleY = 30;
+            double currentPosX = 0;
+            currentPosX += scaleY;
+            while (currentPosX < panel.ActualWidth)
+            {
+                Line line = new Line
+                {
+                    X1 = currentPosX,
+                    Y1 = 0,
+                    X2 = currentPosX,
+                    Y2 = panel.ActualHeight,
+                    Stroke = gridBrush,
+                    StrokeThickness = 0.1
+                };
+                panel.Children.Add(line);
+                
+                currentPosX += scaleY;
+            }
+        }
+
+        public static void DrawCircle(Panel panel)
+        {
+            var centerX = panel.ActualWidth/2;
+            var centerY = panel.ActualHeight/2;
+            double step = 20;
+            double radius = 100;
+            double cursor = 0;
+            var gridBrush = new SolidColorBrush {Color = Colors.Red};
+
+            while (cursor <= radius)
+            {
+                var contour = System.Math.Sqrt(radius * radius - System.Math.Pow(cursor, 2));
+                Line aa = new Line
+                {
+                    X1 = centerX - contour,
+                    Y1 = centerY + cursor,
+                    X2 = centerX + contour,
+                    Y2 = centerY + cursor,
+                    Stroke = gridBrush,
+                    StrokeThickness = 1
+                };
+                panel.Children.Add(aa);
+
+                Line bb = new Line
+                {
+                    X1 = centerX - contour,
+                    Y1 = centerY - cursor,
+                    X2 = centerX + contour,
+                    Y2 = centerY - cursor,
+                    Stroke = gridBrush,
+                    StrokeThickness = 1
+                };
+                panel.Children.Add(bb);
+
+                Line cc = new Line
+                {
+                    X1 = centerX - cursor,
+                    Y1 = centerY - contour,
+                    X2 = centerX - cursor,
+                    Y2 = centerY + contour,
+                    Stroke = gridBrush,
+                    StrokeThickness = 1
+                };
+                panel.Children.Add(cc);
+
+                Line dd = new Line
+                {
+                    X1 = centerX + cursor,
+                    Y1 = centerY - contour,
+                    X2 = centerX + cursor,
+                    Y2 = centerY + contour,
+                    Stroke = gridBrush,
+                    StrokeThickness = 1
+                };
+                panel.Children.Add(dd);
+
+                cursor += step;
+            }
+        }
+    }
+}
---
+++ DrawGrid/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DrawGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SizeChanged += MainWindow_Resize;
+        }
+        
+        private void MainWindow_Resize(object sender, EventArgs e)
+        {
+            MyGrid.Children.Clear();
+            GridTool.Draw(MyGrid);
+            DrawPath(MyGrid);
+        }
+        
+        private readonly Polyline _line = new Polyline();
+        private readonly PointCollection _collection = new PointCollection();
+        private Random rd = new Random();
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _collection.Add(new Point(20,20));
+            _collection.Add(new Point(40,25));
+            _collection.Add(new Point(60,40));
+            _collection.Add(new Point(80,120));
+            _collection.Add(new Point(120,140));
+            _collection.Add(new Point(200,180));
+        }
+
+        private void DrawPath(Panel panel)
+        {
+            _line.Points = _collection;
+            _line.Stroke = new SolidColorBrush(Colors.Black);
+            _line.StrokeThickness = 1;
+            panel.Children.Add(_line);
+        }
+    }
+}
